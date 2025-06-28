6be598efcfdc7105d3260aedc5e54b7665204233
deepanpaudel07 <dodeepan7@gmail.com>
2025-06-28 22:22:53 +0545

    new changes few deletions and few addons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,22 +3,13 @@ import {Routes} from '@angular/router';
 import {HomeComponent} from './features/home/home.component';
 
 export const appRoutes: Routes = [
-  {path: '', component: HomeComponent},// eager route
+  // {path: '', component: HomeComponent},// eager route
   // {path: '', loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent)}, //lazy route
-
   {
-    path: 'main',
-    loadComponent: () => import('./layouts/main-layout/main-layout.component').then(m => m.MainLayoutComponent)
+    path: '',
+    loadComponent: () => import('./layouts/main-layout/main-layout.component').then(m => m.MainLayoutComponent),
+    children: [
+      {path: '', loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent)},
+    ]
   }
-
-  // { path: 'products', component: ProductsComponent },
-  // {
-  //   path: 'seller',
-  //   component: SellerLayoutComponent,
-  //   children: [
-  //     { path: '', redirectTo: 'profile', pathMatch: 'full' },
-  //     { path: 'profile', component: SellerProfileComponent },
-  //     { path: 'orders', component: SellerOrdersComponent }
-  //   ]
-  // },
 ];
--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.css']
 })
